399ae37a6df2c615c501b2e52cc5d79ae9a34d35
Eirik Stien <eirik.stien@bekk.no>
2012-11-03 14:09:07 +0100

    Navigering

---
+++ InstmasWin8App/InstmasWin8App/DayPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using InstmasWin8App.Common;
+using InstmasWin8App.DataModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace InstmasWin8App
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DayPage : LayoutAwarePage
+    {
+        public DayPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void LoadState(object navigationParameter, Dictionary<string, object> pageState)
+        {
+
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.DefaultViewModel["Day"] = new Day{ DayNumber = (int) e.Parameter };
+            base.OnNavigatedTo(e);
+        }
+    }
+}
