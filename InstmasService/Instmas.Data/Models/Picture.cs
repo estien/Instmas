601197dc09dd2e3a1a5bb2f400707772a719c5ab
Lars Skjelbek <lars.skjelbek@gmail.com>
2012-11-03 10:33:09 +0100

    Lagt til prosjekt for shared data

---
+++ InstmasService/Instmas.Data/Models/Picture.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Instmas.Data.Models
+{
+    public class Picture
+    {
+        public string Url { get; set; }
+        public string UserName { get; set; }
+        public DateTime PublishedDate { get; set; }
+    }
+}
---
+++ InstmasService/InstmasService/Controllers/PictureController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using Instmas.Data.Models;
+
+namespace InstmasService.Controllers
+{
+    public class PictureController : ApiController
+    {
+        // GET api/picture
+        public IEnumerable<string> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET api/picture/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/picture
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/picture/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/picture/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
